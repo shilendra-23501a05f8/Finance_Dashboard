9e07c23c14c68c2fa0834c83832f7c1bca2a617f
shilendra-23501a05f8 <23501a05f8@pvpsit.ac.in>
2026-04-05 20:39:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,6 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-const cors = require("cors");
-
-app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://finance-dashboard-f757g9plb-shilendras-projects.vercel.app/login"
-    ],
-    credentials: true
-}));
-
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 });
